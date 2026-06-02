ebffeb96d339ad6cd0fa1024b23f90441c7c418a
Niket <krnike96@gmail.com>
2026-06-03 00:26:55 +0530

    feat: pending work

---
+++ day-7/search.ts
@@ -0,0 +1,70 @@
+interface FOOD{
+    id: number,
+    name: string,
+    price: number
+}
+const foods: FOOD[] = [
+    {
+        id: 1,
+        name: "Momo",
+        price: 100
+    },
+    {
+        id: 2,
+        name: "Briyani",
+        price: 100
+    },
+    {
+        id: 3,
+        name: "Dosa",
+        price: 70
+    },
+    {
+        id: 4,
+        name: "Maggi",
+        price: 30
+    },
+    {
+        id: 5,
+        name: "Idli",
+        price: 50
+    },
+    {
+        id: 6,
+        name: "Fish",
+        price: 200
+    },
+    {
+        id: 7,
+        name: "Chicken",
+        price: 250
+    },
+    {
+        id: 8,
+        name: "Mutton",
+        price: 400
+    },
+    {
+        id: 9,
+        name: "Egg",
+        price: 50
+    },
+    {
+        id: 10,
+        name: "Uppam",
+        price: 120
+    },
+    {
+        id: 11,
+        name: "Pizza",
+        price: 150
+    },
+];
+
+const search_term = 'p';
+
+const filtered_items: FOOD[] = foods.filter((food: FOOD) => {
+    return food.name.toLowerCase().includes(search_term.toLowerCase());
+});
+
+console.log(filtered_items);
